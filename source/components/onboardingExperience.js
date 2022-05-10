ba88f4d3bdd5a6f22aa33b51ecc9dc0a1381e2df
Dagmawi Demissie <dagim.dawit17@gmail.com>
2022-05-10 09:48:08 -0700

    onbordingExperience

---
+++ source/components/onboardingExperience.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {View, Text, Button, Image, StyleSheet} from 'react-native';
+
+import Onboarding from 'react-native-onboarding-swiper';
+
+const Skip = ({...props}) => <Button title="Skip" color="000000" {...props} />;
+
+const Next = ({...props}) => <Button title="Next" color="000000" {...props} />;
+
+const Done = ({...props}) => <Button title="Done" color="000000" {...props} />;
+
+const OnboardingExperience = ({navigation}) => {
+  return (
+    <Onboarding
+      SkipButtonComponent={Skip}
+      NextButtonComponent={Next}
+      DoneButtonComponent={Done}
+      onSkip={() => navigation.navigate('Login')}
+      onDone={() => navigation.navigate('Login')}
+      pages={[
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('./assets/onboarding-img.png')} />,
+          title: 'Onboarding 1',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('./assets/onboarding-img1.png')} />,
+          title: 'Onboarding 2',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('./assets/onboarding-img2.png')} />,
+          title: 'Onboarding 2',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+      ]}
+    />
+  );
+};
+
+export default OnboardingExperience;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
